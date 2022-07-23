51749aa7919a250476ac730396fd8a67f2830a6f
Justin <justin@3wmholdings.com>
2022-07-23 14:24:36 +0200

    first commit

---
+++ test/Inbox.test.js
@@ -0,0 +1,11 @@
+// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+// New instance of Web3
+const web3 = new Web3();
+
+
+
+
